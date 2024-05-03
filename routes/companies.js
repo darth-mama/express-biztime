228cfa62d49ffc1f1b298b8e782f1176f3c2f373
darth-mama <128187728+darth-mama@users.noreply.github.com>
2024-05-03 17:31:56 -0500

    4/4 passed

--- routes/companies.js
+++ routes/companies.js
@@ -63,22 +63,22 @@ router.post("/", async (req, res, next) => {
 });
 // Edit existing company.
 router.put("/:code", async (req, res, next) => {
-  let { name, description } = req.body;
-  let code = req.params.code;
-
   try {
+    let { name, description } = req.body;
+    let code = req.params.code;
+
     const result = await db.query(
       `UPDATE companies
-            SET name=$1, descripton=$2
-            WHERE code=$3)
-
+            SET name=$1, description=$2
+            WHERE code=$3
             RETURNING code, name, description`,
       [name, description, code]
     );
+
     if (result.rows.length === 0) {
-      throw new ExpressError(`${code} does not exist, 404`);
+      throw new ExpressError(`${code} does not exist`, 404);
     } else {
-      return res.status(201).json({ company: result.rows[0] });
+      return res.json({ company: result.rows[0] });
     }
   } catch (e) {
     return next(e);
